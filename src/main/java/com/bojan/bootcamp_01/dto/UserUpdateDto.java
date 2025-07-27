b54805b8e2f2fe07c8b192e5068a108cbc1ecfea
Bojan Dimovski <bojan.dimovski@sebx.se>
2025-07-27 17:15:20 +0000

    update userdto

--- src/main/java/com/bojan/bootcamp_01/dto/UserUpdateDto.java
+++ src/main/java/com/bojan/bootcamp_01/dto/UserUpdateDto.java
@@ -1,14 +1,9 @@
 package com.bojan.bootcamp_01.dto;
 
 import lombok.Data;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.Pattern;
 
 @Data
 public class UserUpdateDto {
-    @Pattern(regexp = "^[a-zA-Z0-9]{3,20}$", message = "Username must be alphanumeric and between 3 and 20 characters long")
     private String username;
-
-    @Email(message = "Email should be valid")
     private String email;
 }
